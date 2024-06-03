1a5709f0fd24a84911dec894ba912247fb849907
CGJock <cjocksanest@gmail.com>
2024-06-03 15:47:17 -0600

    hacer post al api

--- src/index.js
+++ src/index.js
@@ -1,2 +1,44 @@
+let btnAgreagar = document.getElementById("btnAgregar")
+let inputTarea = document.getElementById("inputTarea")
+
+// let tarea = {
+//     task: inputTarea.value
+// }
+// async function getTask() {
+//     try{
+//         const response = await fetch('http://localhost:3000/api/todo')
+//         const data = await response.json()
+//         console.log(data)
+//     } catch(error){
+//         console.log(error)
+//     }
+// } 
+// getTask()
+btnAgreagar.addEventListener("click", function () {
+    asyncPostCall = async () => {
+        try {
+            const response = await fetch('http://localhost:3000/api/todo', {
+             method: 'POST',
+             headers: {
+               'Content-Type': 'application/json'
+               },
+               body: JSON.stringify({
+                // your expected POST request payload goes here
+                        title: inputTarea.value,
+
+                       })
+         // your expected POST request payload goes her
+             });
+             const data = await response.json();
+          // enter you logic when the fetch is successful
+             console.log(data);
+           } catch(error) {
+         // enter your logic for when there is an error (ex. error toast)
+    
+              console.log(error)
+             } 
+        }
+   asyncPostCall()     
+})
 
 
--- src/indexBE.js
+++ src/indexBE.js
@@ -7,10 +7,14 @@ btnAgreagar.addEventListener("click", function addTask() {
 
     let contenedor = document.createElement("div")
     contenedor.className = "contenedorTareas"
-    
     let tarea = document.createElement("p")
     tarea.innerHTML = inputTarea.value
+    let icon = document.createElement("img")
+    icon.src = "/src/img/compartimiento.png"
+    icon.className = "icon"
     contenedor.appendChild(tarea)
-    contenedorPrincipal.appendChild(tarea)
+    contenedor.appendChild(icon)
+    contenedorPrincipal.appendChild(contenedor)
     
 })
+
