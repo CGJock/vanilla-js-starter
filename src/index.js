98e5019d03774244e629cca0ab3add024496bd84
CGJock <cjocksanest@gmail.com>
2024-06-04 15:47:57 -0600

    mostrar tareas en screen

--- src/index.js
+++ src/index.js
@@ -1,49 +1,43 @@
-let btnAgreagar = document.getElementById("btnAgregar")
-let inputTarea = document.getElementById("inputTarea")
+const postTask = async (tarea) => {
+try {
+      const response = await fetch('http://localhost:3000/api/todo', {
+       method: 'POST',
+       headers: {
+         'Content-Type': 'application/json'
+         },
+         body: JSON.stringify({
 
-// let tarea = {
-//     task: inputTarea.value
-// }
-// async function getTask() {
-//     try{
-//         const response = await fetch('http://localhost:3000/api/todo')
-//         const data = await response.json()
-//         console.log(data)
-//     } catch(error){
-//         console.log(error)
-//     }
-// } 
-// getTask()
-btnAgreagar.addEventListener("click", function () {
-    asyncPostCall = async () => {//funcion que hace un llamado al api
-        try {
-            const response = await fetch('http://localhost:3000/api/todo', {
-             method: 'POST',//aca se especifica el methodo a usar en el api
-             headers: {
-               'Content-Type': 'application/json'
-               },
-               body: JSON.stringify({
-                // el body va a ser igual a un objeto
-                        title: inputTarea.value,
+           tarea: tarea,
+      
+          })
+       });
+       const data = await response.json()
+     } catch(error) {
 
-                       })
-                       
+
+        console.log(error)
+       } 
+  }
+
+  const getTask  = async() => {
+    try{
+      const response = await fetch('http://localhost:3000/api/todo');
+      const data = await response.json()
+      return data
       
-             });
-             const data = await response.json();//data que contiene la respuesta del servidor 
-             data.forEach(element => {
-              console.log(element.title)
-              return element.title
-             });
-      
-           } catch(error) {
-         // enter your logic for when there is an error (ex. error toast)
+    } catch(error){
+      console.log(error)
+    }
     
-              console.log(error)
-             } 
-        }
-   asyncPostCall()     
-})
+  }
 
 
-elem
+
+
+
+
+
+
+
+export {postTask,getTask}
+   
--- src/indexBE.js
+++ src/indexBE.js
@@ -1,13 +1,47 @@
-let inputTarea = document.getElementById("inputTarea")
-let btnAgreagar = document.getElementById("btnAgregar")
+import { postTask,getTask } from "./index.js"
+
+let inputTarea = document.getElementById("inputTarea");
+let btnAgreagar = document.getElementById("btnAgregar");
 let contenedorPrincipal = document.getElementById("contenedorPrincipal")
 
+async function obtenerTareas() {
+     let datos = await getTask()
+    datos.forEach(element => {
+        
+        let contenedor = document.createElement("div")
+    contenedor.className = "contenedorTareas"
+    
+    let checkbox = document.createElement("input")
+    checkbox.type = "checkbox";
+    checkbox.id = "checkbox"
+    
+    let tarea = document.createElement("p")
+    tarea.innerHTML = element.tarea
+    
+    let icon = document.createElement("img")
+    icon.src = "/src/img/compartimiento.png"
+    icon.className = "icon"
+   
+    contenedor.appendChild(checkbox)
+    contenedor.appendChild(tarea)
+    contenedor.appendChild(icon)
+    contenedorPrincipal.appendChild(contenedor)
+    });
+}
 
+///////////////////////
+window.addEventListener("load",(event) => {
+    obtenerTareas()
+}
+)
+///////////////////////////////////////////////
 btnAgreagar.addEventListener("click", function addTask() {
-    if (inputTarea.value === "") {
+
+    
+    if (inputTarea.value == "") {
         alert("se debe ingresar texto")
         return false
-    }
+    } else{
 
     let contenedor = document.createElement("div")
     contenedor.className = "contenedorTareas"
@@ -27,5 +61,10 @@ btnAgreagar.addEventListener("click", function addTask() {
     contenedor.appendChild(tarea)
     contenedor.appendChild(icon)
     contenedorPrincipal.appendChild(contenedor)
+
+
+    postTask(inputTarea.value)
+}
+
     })
 
