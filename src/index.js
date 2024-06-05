56f5cf661c7f1087dbd8fbe3919a0bcd563358d3
CGJock <cjocksanest@gmail.com>
2024-06-05 15:45:59 -0600

    del task implementado y gettask pulido

--- src/index.js
+++ src/index.js
@@ -8,10 +8,12 @@ try {
          body: JSON.stringify({
 
            tarea: tarea,
+           estado: 'incompleto'
       
           })
        });
        const data = await response.json()
+       window.location.reload()
      } catch(error) {
 
 
@@ -23,6 +25,7 @@ try {
     try{
       const response = await fetch('http://localhost:3000/api/todo');
       const data = await response.json()
+      
       return data
       
     } catch(error){
@@ -30,14 +33,48 @@ try {
     }
     
   }
+//utiliza el parametro id que es igual al id de los contenedores
+  const delTask = async(id) => {
+    try {
+      const response = await fetch(`http://localhost:3000/api/todo/${id}`, {
+       method: 'DELETE',//llama al metodo que se va a usar 
+       headers: {
+         'Content-Type': 'application/json'
+         },
+         
+       });
+       const data = response.json()//obtiene la data convertida a json
+       window.location.reload()//se recarga cada vez que la pagina se recarga
+       return data
+      
+     } catch(error) {
+      
+        console.log("no se puede borrar")
+       } 
+  }
+
+  const putTask = async(tarea) => {
+    try {
+      const response = await fetch('http://localhost:3000/api/todo', {
+       method: 'PUT',
+       headers: {
+         'Content-Type': 'application/json'
+         },
+         body: JSON.stringify({
+
+           tarea: tarea,
+           estado: incompleta
+      
+          })
+       });
+       const data = await response.json()
+       return data
+       
+     } catch(error) {
 
 
-
-
-
-
-
-
-
-export {postTask,getTask}
+        console.log(error)
+       } 
+  }
+  export {postTask,getTask,delTask,putTask}
    
--- src/indexBE.js
+++ src/indexBE.js
@@ -1,70 +1,75 @@
-import { postTask,getTask } from "./index.js"
+import { postTask, getTask, delTask, putTask } from "./index.js";
 
 let inputTarea = document.getElementById("inputTarea");
 let btnAgreagar = document.getElementById("btnAgregar");
-let contenedorPrincipal = document.getElementById("contenedorPrincipal")
+let contenedorPrincipal = document.getElementById("contenedorPrincipal");
 
 async function obtenerTareas() {
-     let datos = await getTask()
-    datos.forEach(element => {
+    
+    let datos = await getTask();
+    datos.forEach((element) => {//for each usado para recorrer todos los datos del api
+    let contenedor = document.createElement("div");
+    contenedor.className = "contenedorTareas";
+    contenedor.id = element.id//contenedor id va a ser igual al id del elemento
+    id = element.id//ahora el id va a ser igual a elemento, se envia esto como parametro
+    //gracias a eso sabemos cual es el id que queremos enviar 
+    console.log(element.tarea,id,element.estado);
+
+
+    let checkbox = document.createElement("input");
+    checkbox.type = "checkbox";//bloque de codigo que crea el checkbox
+    checkbox.id = "checkbox";
+    checkbox.addEventListener("click", () => {
+        data.estado = "completa"
+        putTask(contenedor.id)//se debe meter el addevent listener dentro de la funcion 
         
-        let contenedor = document.createElement("div")
-    contenedor.className = "contenedorTareas"
-    
-    let checkbox = document.createElement("input")
-    checkbox.type = "checkbox";
-    checkbox.id = "checkbox"
-    
-    let tarea = document.createElement("p")
-    tarea.innerHTML = element.tarea
-    
-    let icon = document.createElement("img")
-    icon.src = "/src/img/compartimiento.png"
-    icon.className = "icon"
-   
-    contenedor.appendChild(checkbox)
-    contenedor.appendChild(tarea)
-    contenedor.appendChild(icon)
-    contenedorPrincipal.appendChild(contenedor)
+      });
+
+    let tarea = document.createElement("p");
+    tarea.innerHTML = element.tarea;
+
+    let icon = document.createElement("img");
+    icon.src = "http://localhost:1234/compartimiento.97bc848e.png";
+    icon.class = "icon"
+    icon.addEventListener("click", () => {
+      delTask(contenedor.id)//se debe meter el addevent listener dentro de la funcion 
     });
+
+    contenedor.appendChild(checkbox);
+    contenedor.appendChild(tarea);
+    contenedor.appendChild(icon);
+    contenedorPrincipal.appendChild(contenedor);
+  });
 }
 
-///////////////////////
-window.addEventListener("load",(event) => {
-    obtenerTareas()
-}
-)
-///////////////////////////////////////////////
+obtenerTareas()
+
 btnAgreagar.addEventListener("click", function addTask() {
+  if (inputTarea.value.trim() == "") {
+    alert("se debe ingresar texto");
+    return false;
+  } else {
+    let contenedor = document.createElement("div");
+    contenedor.className = "contenedorTareas";//se se da ese classname para poder editarlo 
 
-    
-    if (inputTarea.value == "") {
-        alert("se debe ingresar texto")
-        return false
-    } else{
-
-    let contenedor = document.createElement("div")
-    contenedor.className = "contenedorTareas"
-    
-    let checkbox = document.createElement("input")
+    let checkbox = document.createElement("input");//creacion de un imput checkbox
     checkbox.type = "checkbox";
-    checkbox.id = "checkbox"
-    
-    let tarea = document.createElement("p")
-    tarea.innerHTML = inputTarea.value
-    
-    let icon = document.createElement("img")
-    icon.src = "/src/img/compartimiento.png"
-    icon.className = "icon"
-   
-    contenedor.appendChild(checkbox)
-    contenedor.appendChild(tarea)
-    contenedor.appendChild(icon)
-    contenedorPrincipal.appendChild(contenedor)
+    checkbox.id = "checkbox";
 
+    let tarea = document.createElement("p");//crea una eqtiqueta p
+    tarea.innerHTML = inputTarea.value;//le va dar un valor al inner la etiquet 
 
-    postTask(inputTarea.value)
-}
+    let icon = document.createElement("img");
+    icon.src = "http://localhost:1234/compartimiento.97bc848e.png";
+    icon.class = "icon";
 
-    })
+    contenedor.appendChild(checkbox);//le ingresa un checbox a los contenedores
+    contenedor.appendChild(tarea);//le agrega el valor del input al contenedor "p"
+    contenedor.appendChild(icon);
+    contenedorPrincipal.appendChild(contenedor);
+
+    postTask(inputTarea.value);
+    inputTarea.value = ""//Devuelve el valor de input a "vacio"
+  }
+});
 
